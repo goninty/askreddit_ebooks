24cc6ce124934eef499e092e033a25026e17824c
Andrew <andrewmorton2@hotmail.co.uk>
2019-04-27 11:12:50 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- ebooks.py
+++ ebooks.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             print(">>> Generating from {0}".format(file))
             string_list = open(file).readlines()
             for item in string_list:
-                source_statuses += item.split(",")
+                source_statuses += item.split("\n") #not csv anymore, eat my ass senpai
         if SCRAPE_URL:
             source_statuses += scrape_page(SRC_URL, WEB_CONTEXT, WEB_ATTRIBUTES)
         if ENABLE_TWITTER_SOURCES and TWITTER_SOURCE_ACCOUNTS and len(TWITTER_SOURCE_ACCOUNTS[0]) > 0:
