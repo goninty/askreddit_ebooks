72150c98d7d0a44e0cd82763dd34e584eaf94f32
Andrew <andrewmorton2@hotmail.co.uk>
2019-07-14 07:37:52 +0100

    removed all extra unneeded stuff, mainly in local_settings.py

--- ebooks.py
+++ ebooks.py
@@ -2,7 +2,6 @@ import random
 import re
 import sys
 import twitter
-from mastodon import Mastodon
 import markov
 from bs4 import BeautifulSoup
 from local_settings import *
@@ -19,14 +18,12 @@ except ImportError:
     chr = unichr
     from local_settings import *
 
-def connect(type='twitter'):
-    if type == 'twitter':
-        return twitter.Api(consumer_key=MY_CONSUMER_KEY,
+def connect(): #connect to twitter api for bot
+    return twitter.Api(consumer_key=MY_CONSUMER_KEY,
                        consumer_secret=MY_CONSUMER_SECRET,
                        access_token_key=MY_ACCESS_TOKEN_KEY,
                        access_token_secret=MY_ACCESS_TOKEN_SECRET,
                        tweet_mode='extended')
-    return None
 
 def entity(text):
     if text[:2] == "&#":
@@ -60,34 +57,37 @@ def filter_status(text):
 if __name__ == "__main__":
     get_titles() #get the askreddit titles, this func exists in get_askreddit_data.py
     
-    order = ORDER
+    order = ORDER #the "sensical-ness" value from local_settings.py
     guess = 0
     if ODDS and not DEBUG: #if not in debug, give it a roll
         guess = random.randint(0, ODDS - 1)
 
     if guess:
         print(str(guess) + " No, sorry, not this time.")  # message if the random number fails.
-        sys.exit()
+        sys.exit() #bye bye
     else:
+        #connect to twitter api, initalize source list
         api = connect()
-        source_statuses = []
+        source_titles = []
         
-        if STATIC_TEST:
-            file = TEST_SOURCE
-            print(">>> Generating from {0}".format(file))
-            string_list = open(file).readlines()
-            for item in string_list:
-                item = filter_status(item)
-                source_statuses += item.split("\n") #not csv anymore, eat my ass senpai
+        file = SOURCE
+        print(">>> Generating from {0}".format(file))
+        string_list = open(file).readlines()
+        for item in string_list:
+            item = filter_status(item)
+            source_titles += item.split("\n") #not csv anymore, eat my ass senpai
 
-        if len(source_statuses) == 0:
+        if len(source_titles) == 0:
             print("No statuses found!")
             sys.exit()
+        
         mine = markov.MarkovChainer(order)
-        for status in source_statuses:
+        
+        for status in source_titles:
             if not re.search('([\.\!\?\"\']$)', status):
                 status += "?" #add a question mark at the end if one not found to keep the askreddit question thing consistent
             mine.add_text(status)
+        
         for x in range(0, 10):
             ebook_status = mine.generate_sentence()
 
@@ -96,8 +96,8 @@ if __name__ == "__main__":
         
         if ebook_status is not None and len(ebook_status) < 210:
             while similar:
-                for status in source_statuses:
-                    if ebook_status[:-1] not in status:
+                for status in source_titles:
+                    if ebook_status[:-1] not in status: #remove newline char and check if similar
                         #if its something unique, cool we've got the status and can proceed
                         similar = False
                         continue
